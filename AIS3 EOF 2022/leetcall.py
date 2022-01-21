1a74fe00e156ca385a599d6f9d7e55f32be14a43
i1011 <56438495+i1011@users.noreply.github.com>
2022-01-21 23:30:47 +0800

    Create leetcall.py

---
+++ AIS3 EOF 2022/leetcall.py
@@ -0,0 +1,5 @@
+print(getattr("Hello, {0}!","format")(getattr(getattr(open(0),"read")(),"replace")("\n","!\nHello, ")))
+
+print(getattr("\n","join")(list(map(str,map(getattr(list(map(round,map(getattr(0.4472135954999579,"__mul__"),map(getattr(1.618033988749895,"__pow__"), range(71))))),"__getitem__"),map(int,getattr(getattr(open(0),"read")(),"split")()))))))
+
+print(getattr("\n","join")(map(getattr(list(map(getattr("","join"),zip(map(getattr("Fizz","__mul__"),map(int,getattr("100","__mul__")(9999))),map(getattr("Buzz","__mul__"),map(int,getattr("10000","__mul__")(9999))),getattr(getattr(getattr(".","join")(map(str,map(int,map(float,map(getattr("","join"),zip(getattr(".00.0..00..0.00","__mul__")(999),map(str,range(10002)))))))),"replace")(".0","."),"split")(".")))),"__getitem__"),range(1,10001))))
